7eddba056a0d780966a8b7dfb86b971a0323838d
zhujian <2240610682@qq.com>
2017-12-11 17:02:30 +0800

    12/10

---
+++ src/Stack/Sqstack.java
@@ -0,0 +1,39 @@
+package Stack;
+
+/**
+ * 顺序栈
+ */
+public class Sqstack implements IStack{
+    private Object[] stackElem;//对象数组
+    private int top;//在非空栈中，top始终指向栈顶元素的下一个存储位置，当栈为空时，top值为0；
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Boolean isEmpty() {
+        return null;
+    }
+
+    @Override
+    public int length() {
+        return 0;
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+    @Override
+    public void push(Object x) throws Exception {
+
+    }
+
+    @Override
+    public Object pop() {
+        return null;
+    }
+}
